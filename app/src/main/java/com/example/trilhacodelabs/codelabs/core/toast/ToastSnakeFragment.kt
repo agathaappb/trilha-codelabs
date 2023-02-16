92ff405ddebff9550344f6dbaf20fddee1450547
agathaappb <agatha.alana@hotmail.com>
2023-02-16 07:32:45 -0300

    Definindo orientação de tela
    -> definindo orientação da activity base das fragments
    -> definindo orientação específica para a fragment

--- app/src/main/java/com/example/trilhacodelabs/codelabs/core/toast/ToastSnakeFragment.kt
+++ app/src/main/java/com/example/trilhacodelabs/codelabs/core/toast/ToastSnakeFragment.kt
@@ -1,9 +1,24 @@
 package com.example.trilhacodelabs.codelabs.core.toast
 
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.trilhacodelabs.R
+import com.example.trilhacodelabs.databinding.FragmentToastSnakeBinding
 
 class ToastSnakeFragment : Fragment(R.layout.fragment_toast_snake) {
+
+    private lateinit var binding: FragmentToastSnakeBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentToastSnakeBinding.bind(view)
+
+        //definindo orientação da fragment
+        requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+    }
 
 
 }
