1fc36bb2dbc96226c2d2c9c31b58523dc399c674
agathaappb <agatha.alana@hotmail.com>
2023-02-15 21:48:09 -0300

    Criando toast fragment
    -> criando o fragment para o toast e o snake
    -> adicionando novo fragment no arquivo de gráfico de navegação
    -> definindo ação de navegação para o novo fragment.

---
+++ app/src/main/java/com/example/trilhacodelabs/codelabs/core/toast/ToastSnakeFragment.kt
@@ -0,0 +1,9 @@
+package com.example.trilhacodelabs.codelabs.core.toast
+
+import androidx.fragment.app.Fragment
+import com.example.trilhacodelabs.R
+
+class ToastSnakeFragment : Fragment(R.layout.fragment_toast_snake) {
+
+
+}
